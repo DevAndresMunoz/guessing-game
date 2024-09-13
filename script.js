94be3efed1f8792de8f8ae13a4515fc6ac293852
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-09-13 17:12:58 -0600

    declared variables and prompted user for name / guess

---
+++ script.js
@@ -0,0 +1,10 @@
+let secretNumber = 2;
+
+let numberOfAttempts = 0;   
+
+let attemptLimit = 3;
+
+let userName = prompt("Welcome! What is your name?");
+
+let guess = prompt(`Welcome ${userName}! Please guess a number from 1 to 50. You have three guesses remaining:`);
+
