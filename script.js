3f62a8a1666bc301428a41c1e1aafc36e8a4c9ac
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-09-15 19:12:59 -0600

    repeated if else statemtns to allow one input error per guess

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ let userName = prompt("Welcome! What is your name?");
 let guess = prompt(`Welcome ${userName}! Please guess a number from 1 to 50. You have ${attempsRemaining} guesses remaining:`);
 
 if (isNaN(guess)) {
-    guess = prompt(`Uh oh, that's not a number! Please try again. You have ${attempsRemaining} guesses remaining.`)
+    guess = prompt(`Uh oh, that's not a number! Please try again. You have ${attempsRemaining} guesses remaining.`);
+    if (guess == secretNumber) {
+        alert(`You guessed correctly! The secret number is ${secretNumber}! Please refresh this page to play again.`);
+        } else if (guess > secretNumber) {
+        --attempsRemaining;
+        guess = prompt(`Sorry ${userName}, that number is too high! Please guess again. You have ${attempsRemaining} guesses remaining.`);
+        } else {
+        --attempsRemaining;
+        guess = prompt(`Sorry ${userName}, that number is too low! Please guess again. You have ${attempsRemaining} guesses remaining.`);
+        } 
 } else {
     if (guess == secretNumber) {
         alert(`You guessed correctly! The secret number is ${secretNumber}! Please refresh this page to play again.`);
@@ -21,7 +30,16 @@ if (isNaN(guess)) {
 }
 
 if (isNaN(guess)) {
-    guess = prompt(`Uh oh, that's not a number! Please try again. You have ${attempsRemaining} guesses remaining.`)
+    guess = prompt(`Uh oh, that's not a number! Please try again. You have ${attempsRemaining} guesses remaining.`);
+    if (guess == secretNumber) {
+        alert(`You guessed correctly! The secret number is ${secretNumber}! Please refresh this page to play again.`);
+        } else if (guess > secretNumber) {
+        --attempsRemaining;
+        guess = prompt(`Sorry ${userName}, that number is too high! Please guess again. You have ${attempsRemaining} guesses remaining.`);
+        } else {
+        --attempsRemaining;
+        guess = prompt(`Sorry ${userName}, that number is too low! Please guess again. You have ${attempsRemaining} guesses remaining.`);
+        } 
 } else {
     if (guess == secretNumber) {
         alert(`You guessed correctly! The secret number is ${secretNumber}! Please refresh this page to play again.`);
@@ -35,7 +53,16 @@ if (isNaN(guess)) {
 }
 
 if (isNaN(guess)) {
-    guess = prompt(`Uh oh, that's not a number! Please try again. You have ${attempsRemaining} guesses remaining.`)
+    guess = prompt(`Uh oh, that's not a number! Please try again. You have ${attempsRemaining} guesses remaining.`);
+    if (guess == secretNumber) {
+        alert(`You guessed correctly! The secret number is ${secretNumber}! Please refresh this page to play again.`);
+        } else if (guess > secretNumber) {
+        --attempsRemaining;
+        guess = prompt(`Sorry ${userName}, that number is too high! Please guess again. You have ${attempsRemaining} guesses remaining.`);
+        } else {
+        --attempsRemaining;
+        guess = prompt(`Sorry ${userName}, that number is too low! Please guess again. You have ${attempsRemaining} guesses remaining.`);
+        }
 } else {
     if (guess == secretNumber) {
         alert(`You guessed correctly! The secret number is ${secretNumber}! Please refresh this page to play again.`);
